83c145f9419cf6fa8c4d2f4ad1ab308e1f32eedd
whitezaddy <austinihueze@gmail.com>
2025-08-26 00:22:03 +0100

    Add initial styling and structure for registration form

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ body.style.display = "flex";
 body.style.justifyContent = "center";
 body.style.alignItems = "center";
 body.style.height = "100vh";
-body.style.backgroundColor = "antiqueWhite";
+body.style.backgroundColor = "white";
 
 // style container
 formContainer.style.width = "150px";
@@ -19,9 +19,9 @@ formContainer.style.padding = "5px";
 formContainer.style.backgroundColor = "white";
 formContainer.style.borderRadius = "10px";
 formContainer.style.border = "1px solid #ccc";
-formContainer.style.fontFamily = "Arial, sans-serif"
+formContainer.style.fontFamily = "Arial, sans-serif";
 formContainer.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
-formContainer.style.color = "black"
+formContainer.style.color = "black";
 
 // style h3
 h3.style.textAlign = "center";
@@ -29,29 +29,24 @@ h3.style.color = "teal";
 // style inputs
 inputs.forEach(input => {
     input.style.padding = "3px";
-    input.style.margin = "5px"
-    input.style.border = "1px solid #ccc"
-    input.style.fontSize = "10px"
-    input.style.borderRadius = "5px"
+    input.style.margin = "5px";
+    input.style.border = "1px solid #ccc";
+    input.style.fontSize = "10px";
+    input.style.borderRadius = "5px";
 });
 
 // style button
 Array.from(button).forEach(button => {
-button.style.padding = "5px 10px"
-button.style.marginTop = "10px"
-button.style.border = "none"
-button.style.background = "none"
-button.style.borderRadius = "20px"
-button.style.fontSize = "10px"
-button.style.cursor = "pointer"
-button.style.color = "white"
-button.style.background = "teal"
+button.style.padding = "5px 10px";
+button.style.marginTop = "10px";
+button.style.border = "none";
+button.style.borderRadius = "20px";
+button.style.fontSize = "10px";
+button.style.cursor = "pointer";
+button.style.color = "white";
+button.style.background = "teal";
 });
-button[0].style.color = "teal"
-button[0].style.background = "none"
-button[0].style.fontSize = "7px"
-button[1].style.marginLeft = "23px"
-
-
-
-
+button[0].style.color = "teal";
+button[0].style.background = "none";
+button[0].style.fontSize = "7px";
+button[1].style.marginLeft = "23px";
