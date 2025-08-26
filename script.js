1a5f8cea7944a68fd0dded36b9c0f7ae50fc87d7
whitezaddy <austinihueze@gmail.com>
2025-08-26 11:44:08 +0100

    Updated project with README and new changes

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const body = document.body;
 const formContainer = document.getElementById("form-container");
 const inputs = document.querySelectorAll("input");
 const button = document.getElementsByClassName("button");
-const h3 = document.querySelector("h3");
+const buttons = document.getElementById("buttons");
+const h2 = document.querySelector("h2");
 
 // style body
 body.style.display = "flex";
@@ -13,7 +14,10 @@ body.style.height = "100vh";
 body.style.backgroundColor = "white";
 
 // style container
-formContainer.style.width = "150px";
+formContainer.style.display = "flex";
+formContainer.style.flexDirection = "column";
+formContainer.style.width = "300px";
+formContainer.style.height = "330px";
 formContainer.style.margin = "100px";
 formContainer.style.padding = "5px";
 formContainer.style.backgroundColor = "white";
@@ -23,30 +27,32 @@ formContainer.style.fontFamily = "Arial, sans-serif";
 formContainer.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
 formContainer.style.color = "black";
 
-// style h3
-h3.style.textAlign = "center";
-h3.style.color = "teal";
+// style h2
+h2.style.textAlign = "center";
+h2.style.color = "teal";
 // style inputs
 inputs.forEach(input => {
-    input.style.padding = "3px";
+    input.style.padding = "10px";
     input.style.margin = "5px";
     input.style.border = "1px solid #ccc";
-    input.style.fontSize = "10px";
+    input.style.fontSize = "14px";
     input.style.borderRadius = "5px";
 });
 
 // style button
 Array.from(button).forEach(button => {
-button.style.padding = "5px 10px";
+button.style.padding = "10px 20px";
 button.style.marginTop = "10px";
 button.style.border = "none";
 button.style.borderRadius = "20px";
-button.style.fontSize = "10px";
+button.style.fontSize = "14px";
 button.style.cursor = "pointer";
 button.style.color = "white";
 button.style.background = "teal";
 });
 button[0].style.color = "teal";
+button[0].style.display = "flex";
 button[0].style.background = "none";
-button[0].style.fontSize = "7px";
-button[1].style.marginLeft = "23px";
+button[0].style.fontSize = "10px";
+buttons.style.textAlign = "center";
+buttons.style.marginTop = "10px";
